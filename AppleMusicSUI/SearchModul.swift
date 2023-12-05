399d8debe4e676dde37c5c5f262c1d665eaf1297
sofiigorevna <sofia.konchencko@yandex.ru>
2023-12-05 03:10:39 +0300

    Create TabView, MiniPlayer.

---
+++ AppleMusicSUI/SearchModul.swift
@@ -0,0 +1,20 @@
+//
+//  SearchModul.swift
+//  AppleMusicSUI
+//
+//  Created by 1234 on 04.12.2023.
+//
+
+import SwiftUI
+
+struct SearchModul: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchModul_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchModul()
+    }
+}
