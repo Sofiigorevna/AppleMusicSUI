99cdf4c97872fb3fbddf7184897a669422bf3b28
sofiigorevna <sofia.konchencko@yandex.ru>
2023-12-14 22:40:04 +0300

    renaming ContentView.

--- AppleMusicSUI/MainModul.swift
+++ AppleMusicSUI/MainModul.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainModul: View {
     @State var current = 0
     
     // miniplyer properties
@@ -54,8 +54,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainModul_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainModul()
     }
 }
